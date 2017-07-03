b86f2925bf014937cd3c4b5411d4af8f953d8a08
kaviyarasan-s <kaviyarasan150697@gmail.com>
2017-07-03 18:06:50 +0530

    Create digit.java

---
+++ digit.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class digit
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String s=String.valueOf(n);
+        System.out.print(s.length());
+    }
+}
